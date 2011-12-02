b92c4e89c3e03f914394076333ebf5ff3fc12a16
Goran Novotny <goran@abit.hr>
2011-12-02 11:49:27 +0100

    Combo za postaje

--- unit1.pas
+++ unit1.pas
@@ -5,14 +5,21 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  Classes, SysUtils, sqlite3conn, sqldb, db, FileUtil, Forms, Controls,
+  Graphics, Dialogs, StdCtrls, DbCtrls, DBGrids;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    Datasource1: TDatasource;
+    DBGrid1: TDBGrid;
+    DBLookupComboBox1: TDBLookupComboBox;
     Label1: TLabel;
+    SQLite3Connection1: TSQLite3Connection;
+    SQLQuery1: TSQLQuery;
+    SQLTransaction1: TSQLTransaction;
   private
     { private declarations }
   public
