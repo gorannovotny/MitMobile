c85a904a68583896d47baa5d8c539aaaedabedd2
Goran Novotny <goran@abit.hr>
2011-12-05 16:19:33 +0100

    Array of Controls

--- unit1.pas
+++ unit1.pas
@@ -6,20 +6,21 @@ interface
 
 uses
   Classes, SysUtils, sqlite3conn, sqldb, db, FileUtil, Forms, Controls,
-  Graphics, Dialogs, StdCtrls, DbCtrls, DBGrids;
+  Graphics, Dialogs, StdCtrls, DbCtrls, DBGrids, ExtCtrls;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    Button1: TButton;
     Datasource1: TDatasource;
-    DBGrid1: TDBGrid;
-    DBLookupComboBox1: TDBLookupComboBox;
     Label1: TLabel;
+    Panel1: TPanel;
     SQLite3Connection1: TSQLite3Connection;
     SQLQuery1: TSQLQuery;
     SQLTransaction1: TSQLTransaction;
+    procedure Button1Click(Sender: TObject);
   private
     { private declarations }
   public
@@ -27,11 +28,27 @@ type
   end; 
 
 var
-  Form1: TForm1; 
+  Form1: TForm1;
+  FirstNum:array [0..3] of TPanel;
 
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  FirstNum[0] := TPanel.Create(Self);
+  FirstNum[0].Parent := Self;
+  FirstNum[0].Top := 50;
+  FirstNum[0].Left:= 50;
+  FirstNum[0].Height:= 50;
+  FirstNum[0].Width:= 350;
+  FirstNum[0].Enabled:= true;
+  FirstNum[0].Caption:= 'IdeŠŠŠŠŠŠŠŠŠŠŠŠŠŠŠ';
+  FirstNum[0].Show;
+end;
 
 end.
 
