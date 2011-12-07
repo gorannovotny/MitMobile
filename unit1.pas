d63e02e78f954e89ec4e1954c2f2f54d3c8bcb15
Goran Novotny <gorannovotny@gmail.com>
2011-12-07 22:37:23 +0100

    U dobrom smjeru

--- unit1.pas
+++ unit1.pas
@@ -5,13 +5,12 @@ unit Unit1;
 interface
 
 uses
-  Forms, StdCtrls, ExtCtrls, Classes, Unit2,Unit3, db, ZConnection, ZDataset;
+  Forms, StdCtrls, ExtCtrls, db, ZConnection, ZDataset,Picker;
 
 type
-
   { TForm1 }
-
   TForm1 = class(TForm)
+    Button1: TButton;
     Datasource1: TDatasource;
     Label1: TLabel;
     Label2: TLabel;
@@ -20,60 +19,31 @@ type
     Panel2: TPanel;
     ZConnection1: TZConnection;
     ZQuery1: TZQuery;
+    ZReadOnlyQuery1: TZReadOnlyQuery;
 
+    procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-    procedure Panel1Click(Sender: TObject);
-    procedure Panel2Click(Sender: TObject);
-  private
-    { private declarations }
-  public
-    { public declarations }
-  end; 
+
+  end;
 
 var
   Form1: TForm1; 
-  PanelList : array of TPanel;
-implementation
+  a : TPicker;
 
+implementation
 {$R *.lfm}
 
-{ TForm1 }
-
-{ TForm1 }
-
-
-
-{ TForm1 }
-
-procedure TForm1.Panel1Click(Sender: TObject);
-var a : TPanel;
+procedure TForm1.FormCreate(Sender:TObject);
 begin
-     a := TPanel(Sender);
-     ZQuery1.Open;
-     Form2.Panel1.Caption:= ZQuery1.DbcResultSet.GetString(1);
-     ZQuery1.DbcResultSet.Next;
-     Form2.Panel2.Caption := ZQuery1.DbcResultSet.GetString(1);
-     Form2.Left:= a.Parent.Left + a.Left;
-     Form2.Top := a.Parent.Top + a.Top + 30;
-     Form2.Show;
+
+  a := TPicker.Create;
+  a.Prepare(Self);
+
 end;
 
-procedure TForm1.FormCreate(Sender: TObject);
-  var
-    x: Integer;
-  begin
-     SetLength(PanelList,10);
-     for x:=0 to 9 do
-     begin
-       PanelList[x] := TPanel.Create(self);
-       PanelList[x].Parent := Self;
-       PanelList[x].Visible:= false;
-     end;
-  end;
-
-procedure TForm1.Panel2Click(Sender: TObject);
+procedure TForm1.Button1Click(Sender: TObject);
 begin
-  Unit2.Close(PanelList);
+  a.Show(Sender as TButton);
 end;
 
 end.
