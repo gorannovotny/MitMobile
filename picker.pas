223ba77bbb674e5e40fb1641e6736c042882ff12
Goran Novotny <goran@abit.hr>
2011-12-15 11:00:22 +0100

    Dodana baza

--- picker.pas
+++ picker.pas
@@ -5,7 +5,8 @@ unit Picker;
 interface
 
 uses
-  Forms, StdCtrls, ExtCtrls,SysUtils,Controls,Classes,Graphics;
+  Forms, StdCtrls, ExtCtrls, SysUtils, Controls, Classes, Graphics, ZDataset,
+  ZConnection;
 
 const MaxPicks = 5;
 type
@@ -13,9 +14,12 @@ type
   { TPicker }
 
   TPicker = class (TForm)
+    ZConnection1: TZConnection;
+    ZReadOnlyQuery1: TZReadOnlyQuery;
     procedure FormCreate(Sender: TObject);
   private
     Panels : array of TPanel;
+
     procedure Click(Sender : TObject);
     procedure MouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
     procedure MouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
@@ -42,10 +46,6 @@ begin
     Panels[x].OnClick:= @Click;
     Panels[x].OnMouseDown:= @MouseDown;
     Panels[x].OnMouseUp:= @MouseUp;
-    Panels[x].Caption :=  IntToStr(x) + '. Test';
-    Panels[x].Top := ClientOrigin.y + Panels[x].Height * x;
-    Panels[x].Left := ClientOrigin.x;
-    Panels[x].Show;
   end;
 end;
 
@@ -54,23 +54,25 @@ procedure TPicker.Pokazi(gumb: TButton);
 var x:Integer;
 begin
   ParentGumb := gumb;
-  Self.Top := gumb.Height + gumb.Top + gumb.parent.ClientOrigin.Y;
-  Self.Left:= gumb.left + gumb.parent.ClientOrigin.X;
+  PickForm.Top := gumb.Height + gumb.Top + gumb.parent.ClientOrigin.Y;
+  PickForm.Left:= gumb.left + gumb.parent.ClientOrigin.X;
+  ZReadOnlyQuery1.Open;
   for x:= 0 to MaxPicks - 1  do
   begin
-  Panels[x].Caption :=  IntToStr(x) + '. Test';
-  Panels[x].Top :=  Panels[x].Height * x;
-  Panels[x].Left := 0;
-  Panels[x].Show;
+    Panels[x].Caption :=  ZReadOnlyQuery1.FieldByName('Naziv').AsString;
+    Panels[x].Top :=  Panels[x].Height * x;
+    Panels[x].Left := 0;
+    Panels[x].Width:= gumb.Width;
+    ZReadOnlyQuery1.Next;
   end;
-  Self.ShowModal;
+  PickForm.ShowModal;
 end;
 
 procedure TPicker.Click(Sender:TObject);
 var x:Integer;
 begin
      ParentGumb.Caption := (Sender as TPanel).Caption;
-     Self.Close;
+     PickForm.Close;
 end;
 
 procedure TPicker.MouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
